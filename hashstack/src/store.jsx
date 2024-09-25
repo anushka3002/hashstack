fef1a5e295d082c4592ee104587b643abf38dbd7
Anushka Priya <anuhska2457@gmail.com>
2024-09-25 19:13:30 +0530

    removed devtools dependency

--- hashstack/src/store.jsx
+++ hashstack/src/store.jsx
@@ -1,6 +1,5 @@
 import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import { currencyReducer, exchangeReducer, historicExchangeReducer } from './reducer/currencyReducer';
 
 const reducer = combineReducers({
@@ -15,7 +14,7 @@ const middleware = [thunk];
 const store = createStore(
   reducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  applyMiddleware(...middleware)
 );
 
 export default store;
